82520f280f4d952cc17600a191a33a356e0709be
Gabriele-333 <gabriele3infinity@gmail.com>
2024-06-09 18:55:07 +0200

    Compass

--- src/main/java/net/gabriele333/fmtt/FMTTCreativeTabs.java
+++ src/main/java/net/gabriele333/fmtt/FMTTCreativeTabs.java
@@ -1,7 +1,25 @@
+/*
+ * This file is part of From Magic To Tech.
+ * Copyright (c) 2024, Gabriele_333, All rights reserved.
+ *
+ * From Magic To Tech is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * From Magic To Tech is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with From Magic To Tech.  If not, see <http://www.gnu.org/licenses/lgpl>.
+ */
 package net.gabriele333.fmtt;
 
 import net.gabriele333.fmtt.item.FMTTItems;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -13,7 +31,7 @@ public class FMTTCreativeTabs {
             DeferredRegister.create(Registries.CREATIVE_MODE_TAB, fmtt.MOD_ID);
     public static final RegistryObject<CreativeModeTab> fmtt_creative_tab = CREATIVE_MODE_TABS.register("fmtt_creative_tab",
             () -> CreativeModeTab.builder().icon(() -> new ItemStack(FMTTItems.FMTT_ITEM.get()))
-                    //.title(Component.translatable("creativetab.fmtt_creative_tab"))
+                    .title(Component.translatable("creativetab.fmtt_creative_tab"))
                     .displayItems((pParameters, pOutput) -> {
                         pOutput.accept(FMTTItems.CRY_TIME.get());
                         pOutput.accept(FMTTItems.CRY_TECH.get());
@@ -22,6 +40,7 @@ public class FMTTCreativeTabs {
                         pOutput.accept(FMTTItems.CRY_COSMOS.get());
                         pOutput.accept(FMTTItems.CRY_EMPTY.get());
                         pOutput.accept(FMTTItems.FMTT_XP_ITEM.get());
+                        pOutput.accept(FMTTItems.FMTT_COMPASS.get());
                     })
                     .build());
 
--- src/main/java/net/gabriele333/fmtt/item/FMTTItems.java
+++ src/main/java/net/gabriele333/fmtt/item/FMTTItems.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of From Magic To Tech.
+ * Copyright (c) 2024, Gabriele_333, All rights reserved.
+ *
+ * From Magic To Tech is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * From Magic To Tech is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with From Magic To Tech.  If not, see <http://www.gnu.org/licenses/lgpl>.
+ */
 package net.gabriele333.fmtt.item;
 
 import net.gabriele333.fmtt.fmtt;
@@ -31,6 +48,8 @@ public class FMTTItems {
             () -> new Item(new Item.Properties().rarity(Rarity.UNCOMMON).fireResistant()));
     public static final RegistryObject<Item> FMTT_XP_ITEM = ITEMS.register("fmtt_xp_item",
             () -> new Item(new Item.Properties().rarity(Rarity.RARE).fireResistant()));
+    public static final RegistryObject<Item> FMTT_COMPASS = ITEMS.register("fmtt_compass",
+            () -> new Item(new Item.Properties().rarity(Rarity.COMMON).fireResistant()));
 
 
 
