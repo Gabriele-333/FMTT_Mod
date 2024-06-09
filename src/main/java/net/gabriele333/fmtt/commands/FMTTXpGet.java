d3c4f7428c8ed9ab09b33fe0593688989397e081
Gabriele-333 <gabriele3infinity@gmail.com>
2024-06-09 18:54:19 +0200

    MIT -> LGPLv3

--- src/main/java/net/gabriele333/fmtt/commands/FMTTXpGet.java
+++ src/main/java/net/gabriele333/fmtt/commands/FMTTXpGet.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of From Magic To Tech.
+ * Copyright (c) 2024, Gabriele_333, All rights reserved.
+ *
+ * From Magic To Tech is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * From Magic To Tech is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with From Magic To Tech.  If not, see <http://www.gnu.org/licenses/lgpl>.
+ */
 package net.gabriele333.fmtt.commands;
 import net.gabriele333.fmtt.FMTTXP.PlayerFMTTXpProvider;
 import net.minecraft.network.chat.Component;
