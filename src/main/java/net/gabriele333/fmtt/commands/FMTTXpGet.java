acb68db5daa14ad1ed23f43fd29a3c485c89119e
Gabriele-333 <gabriele3infinity@gmail.com>
2024-06-22 19:44:08 +0200

    Updated

--- src/main/java/net/gabriele333/fmtt/commands/FMTTXpGet.java
+++ src/main/java/net/gabriele333/fmtt/commands/FMTTXpGet.java
@@ -15,14 +15,16 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with From Magic To Tech.  If not, see <http://www.gnu.org/licenses/lgpl>.
  */
+
 package net.gabriele333.fmtt.commands;
-import net.gabriele333.fmtt.FMTTXP.PlayerFMTTXpProvider;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+
+import static net.gabriele333.fmtt.FMTTXP.PlayerFMTTXpProvider.PLAYERFMTTXP;
 
 public class FMTTXpGet {
     public FMTTXpGet(CommandDispatcher<CommandSourceStack> dispatcher) {
@@ -39,9 +41,7 @@ public class FMTTXpGet {
 
     private int getPlayerXp(CommandSourceStack source) throws CommandSyntaxException {
         ServerPlayer player = source.getPlayerOrException();
-        player.getCapability(PlayerFMTTXpProvider.player_fmtt_xp).ifPresent(playerFMTTXp -> {
-            player.sendSystemMessage(Component.literal("XP: " + playerFMTTXp.getPlayerFMTTXP()));
-        });
+        player.sendSystemMessage(Component.literal("XP: " + player.getData(PLAYERFMTTXP)));
         return 1;
     }
 }
