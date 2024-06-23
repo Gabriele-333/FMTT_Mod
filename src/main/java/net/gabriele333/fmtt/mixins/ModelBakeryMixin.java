011d7c2359f74add0fe9b14137369dd2b341eb55
Gabriele-333 <gabriele3infinity@gmail.com>
2024-06-23 16:29:06 +0200

    Test

--- src/main/java/net/gabriele333/fmtt/mixins/ModelBakeryMixin.java
+++ src/main/java/net/gabriele333/fmtt/mixins/ModelBakeryMixin.java
@@ -27,11 +27,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import static net.gabriele333.fmtt.fmtt.LOGGER;
+
 @Mixin(ModelBakery.class)
 public class ModelBakeryMixin {
     @Inject(at = @At("HEAD"), method = "getModel", cancellable = true)
     private void getModelHook(ResourceLocation id, CallbackInfoReturnable<UnbakedModel> cir) {
         var model = BuiltInModelHooks.getBuiltInModel(id);
+        //LOGGER.info("Belin Mixin");
 
         if (model != null) {
             cir.setReturnValue(model);
