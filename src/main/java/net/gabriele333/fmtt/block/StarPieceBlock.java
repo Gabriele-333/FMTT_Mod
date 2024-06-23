5e8a06520e9152b5182fcaacb9853fa50628b9d3
Gabriele-333 <gabriele3infinity@gmail.com>
2024-06-23 16:32:44 +0200

    FMTTBlock -> StarPieceBlock (For the future...)

---
+++ src/main/java/net/gabriele333/fmtt/block/StarPieceBlock.java
@@ -0,0 +1,60 @@
+package net.gabriele333.fmtt.block;/*
+ * This file is part of From Magic To Tech.
+ * Copyright (c) 2024, Gabriele_333, All rights reserved.
+ *
+ * From Magic To Tech is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * From Magic To Tech is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with From Magic To Tech.  If not, see <http://www.gnu.org/licenses/lgpl>.
+ */
+
+
+
+import net.gabriele333.fmtt.server.services.FMTTCompassService;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.registries.DeferredBlock;
+
+import java.util.List;
+
+public class StarPieceBlock extends FMTTBlock{
+
+
+    protected StarPieceBlock(Properties props) {
+        super(props);
+    }
+
+
+    @Override
+    public void onPlace(BlockState state, Level level, BlockPos pos, BlockState oldState, boolean isMoving) {
+        if (level instanceof ServerLevel serverLevel) {
+            FMTTCompassService.notifyBlockChange(serverLevel, pos);
+        }
+    }
+
+    @Override
+    public void onRemove(BlockState state, Level level, BlockPos pos, BlockState newState, boolean isMoving) {
+        if (newState.getBlock() == state.getBlock()) {
+            return; // Just a block state change
+        }
+
+        super.onRemove(state, level, pos, newState, isMoving);
+
+        if (level instanceof ServerLevel serverLevel) {
+            FMTTCompassService.notifyBlockChange(serverLevel, pos);
+        }
+    }
+
+}
