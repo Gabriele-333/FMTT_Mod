95cdd7bf3c8acfa04c5c9eb3797ec560f7a5a576
Gabriele-333 <gabriele3infinity@gmail.com>
2024-06-23 16:34:51 +0200

    Compass

---
+++ src/main/java/net/gabriele333/fmtt/server/services/FMTTCompassRegion.java
@@ -0,0 +1,141 @@
+package net.gabriele333.fmtt.server.services;/*
+ * This file is part of From Magic To Tech.
+ * Copyright (c) 2024, Gabriele_333, All rights reserved.
+ *
+ * From Magic To Tech is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * From Magic To Tech is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with From Magic To Tech.  If not, see <http://www.gnu.org/licenses/lgpl>.
+
+ THIS CODE IS COPIED AND MODIFIED FROM: https://github.com/AppliedEnergistics/Applied-Energistics-2/blob/main/src/main/java/appeng/server/services/compass/CompassRegion.java
+ */
+
+import java.util.BitSet;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import net.gabriele333.fmtt.FMTTSavedData;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.saveddata.SavedData;
+
+
+/**
+ * A compass region stores information about the occurrence of skystone blocks in a region of 1024x1024 chunks.
+ */
+final class FMTTCompassRegion extends FMTTSavedData {
+
+    private static final SavedData.Factory<FMTTCompassRegion> FACTORY = new Factory<>(
+            FMTTCompassRegion::new,
+            null);
+
+    /**
+     * The number of chunks that get saved in a region on each axis.
+     */
+    private static final int CHUNKS_PER_REGION = 1024;
+
+    private static final int BITMAP_LENGTH = CHUNKS_PER_REGION * CHUNKS_PER_REGION;
+
+    // Key is the section index, see ChunkAccess.getSections()
+    private final Map<Integer, BitSet> sections = new HashMap<>();
+
+    /**
+     * Gets the name of the save data for a region that has the given coordinates.
+     */
+    private static String getRegionSaveName(int regionX, int regionZ) {
+        return "fmtt_compass_" + regionX + "_" + regionZ;
+    }
+
+    /**
+     * Retrieve the compass region that serves the given chunk position.
+     */
+    public static FMTTCompassRegion get(ServerLevel level, ChunkPos chunkPos) {
+        Objects.requireNonNull(level, "level");
+        Objects.requireNonNull(chunkPos, "chunkPos");
+
+        var regionX = chunkPos.x / CHUNKS_PER_REGION;
+        var regionZ = chunkPos.z / CHUNKS_PER_REGION;
+
+        return level.getDataStorage().computeIfAbsent(
+                FACTORY,
+                getRegionSaveName(regionX, regionZ));
+    }
+
+
+
+    @Override
+    public CompoundTag save(CompoundTag compound, HolderLookup.Provider registries) {
+        for (var entry : sections.entrySet()) {
+            var key = "section" + entry.getKey();
+            if (entry.getValue().isEmpty()) {
+                continue;
+            }
+            compound.putByteArray(key, entry.getValue().toByteArray());
+        }
+        return compound;
+    }
+
+    boolean hasFMTTBlock(int cx, int cz) {
+        var bitmapIndex = getBitmapIndex(cx, cz);
+        for (BitSet bitmap : sections.values()) {
+            if (bitmap.get(bitmapIndex)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    boolean hasFMTTBlock(int cx, int cz, int sectionIndex) {
+        var bitmapIndex = getBitmapIndex(cx, cz);
+        var section = sections.get(sectionIndex);
+        if (section != null) {
+            return section.get(bitmapIndex);
+        }
+        return false;
+    }
+
+    void setHasFMTTBlock(int cx, int cz, int sectionIndex, boolean hasFMTTBlock) {
+        var bitmapIndex = getBitmapIndex(cx, cz);
+        var section = sections.get(sectionIndex);
+        if (section == null) {
+            if (hasFMTTBlock) {
+                section = new BitSet(BITMAP_LENGTH);
+                section.set(bitmapIndex);
+                sections.put(sectionIndex, section);
+                setDirty();
+            }
+        } else {
+            if (section.get(bitmapIndex) != hasFMTTBlock) {
+                setDirty();
+            }
+            // There already was data on this y-section in this region
+            if (!hasFMTTBlock) {
+                section.clear(bitmapIndex);
+                if (section.isEmpty()) {
+                    sections.remove(sectionIndex);
+                }
+                setDirty();
+            } else {
+                section.set(bitmapIndex);
+            }
+        }
+    }
+
+    private static int getBitmapIndex(int cx, int cz) {
+        cx &= CHUNKS_PER_REGION - 1;
+        cz &= CHUNKS_PER_REGION - 1;
+        return cx + cz * CHUNKS_PER_REGION;
+    }
+
+}
---
+++ src/main/java/net/gabriele333/fmtt/server/services/FMTTCompassService.java
@@ -0,0 +1,196 @@
+package net.gabriele333.fmtt.server.services;/*
+ * This file is part of From Magic To Tech.
+ * Copyright (c) 2024, Gabriele_333, All rights reserved.
+ *
+ * From Magic To Tech is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * From Magic To Tech is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with From Magic To Tech.  If not, see <http://www.gnu.org/licenses/lgpl>.
+
+
+ THIS CODE IS COPIED AND MODIFIED FROM: https://github.com/AppliedEnergistics/Applied-Energistics-2/blob/main/src/main/java/appeng/server/services/compass/CompassService.java
+ */
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
+import net.gabriele333.fmtt.block.FMTTBlock;
+import net.gabriele333.fmtt.block.StarPieceBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.chunk.ChunkAccess;
+
+
+public final class FMTTCompassService {
+
+    public record DirectionQuery(ServerLevel level, int cx, int cz, int maxRange) {
+    }
+
+    /**
+     * @param hasResult true if found a target
+     * @param spin      true if should spin
+     * @param radians
+     * @param distance
+     */
+    public record Result(boolean hasResult, boolean spin, double radians, double distance) {
+        public static final Result ON_THE_SPOT = new Result(true, true, -999, 0);
+        public static final Result INDETERMINATE = new Result(false, true, -999, 999);
+    }
+
+    // We use this basic cache to prevent client-side spamming, although the client can request arbitrary
+    // chunk positions and range, if malicious.
+    private static final LoadingCache<DirectionQuery, Result> DIRECTION_CACHE = CacheBuilder.newBuilder()
+            .maximumSize(100)
+            .weakKeys()
+            .expireAfterWrite(5, TimeUnit.SECONDS)
+            .build(new CacheLoader<>() {
+                @Override
+                public Result load(DirectionQuery directionQuery) {
+                    return calculateCompassDirection(directionQuery);
+                }
+            });
+
+    private static final int CHUNK_SIZE = 16;
+
+    public static Result getDirection(ServerLevel level, ChunkPos chunkPos, int maxRange) {
+        return DIRECTION_CACHE.getUnchecked(new DirectionQuery(level, chunkPos.x, chunkPos.z, maxRange));
+    }
+
+    private static Result calculateCompassDirection(DirectionQuery query) {
+        var cr = FMTTCompassRegion.get(query.level(), new ChunkPos(query.cx(), query.cz()));
+        var cx = query.cx();
+        var cz = query.cz();
+
+        // Am I standing on it?
+        if (cr.hasFMTTBlock(cx, cz)) {
+            return Result.ON_THE_SPOT;
+        }
+
+        // spiral outward...
+        for (int offset = 1; offset < query.maxRange(); offset++) {
+            final int minX = cx - offset;
+            final int minZ = cz - offset;
+            final int maxX = cx + offset;
+            final int maxZ = cz + offset;
+
+            int closest = Integer.MAX_VALUE;
+            int chosen_x = cx;
+            int chosen_z = cz;
+
+            for (int z = minZ; z <= maxZ; z++) {
+                if (cr.hasFMTTBlock(minX, z)) {
+                    final int closeness = dist(cx, cz, minX, z);
+                    if (closeness < closest) {
+                        closest = closeness;
+                        chosen_x = minX;
+                        chosen_z = z;
+                    }
+                }
+
+                if (cr.hasFMTTBlock(maxX, z)) {
+                    final int closeness = dist(cx, cz, maxX, z);
+                    if (closeness < closest) {
+                        closest = closeness;
+                        chosen_x = maxX;
+                        chosen_z = z;
+                    }
+                }
+            }
+
+            for (int x = minX + 1; x < maxX; x++) {
+                if (cr.hasFMTTBlock(x, minZ)) {
+                    final int closeness = dist(cx, cz, x, minZ);
+                    if (closeness < closest) {
+                        closest = closeness;
+                        chosen_x = x;
+                        chosen_z = minZ;
+                    }
+                }
+
+                if (cr.hasFMTTBlock(x, maxZ)) {
+                    final int closeness = dist(cx, cz, x, maxZ);
+                    if (closeness < closest) {
+                        closest = closeness;
+                        chosen_x = x;
+                        chosen_z = maxZ;
+                    }
+                }
+            }
+
+            if (closest < Integer.MAX_VALUE) {
+                return new Result(true, false, rad(cx, cz, chosen_x, chosen_z),
+                        dist(cx, cz, chosen_x, chosen_z));
+            }
+        }
+
+        // didn't find shit...
+        return Result.INDETERMINATE;
+    }
+
+    public static void updateArea(ServerLevel level, ChunkAccess chunk) {
+        var compassRegion = FMTTCompassRegion.get(level, chunk.getPos());
+
+        for (var i = 0; i < level.getSectionsCount(); i++) {
+            updateArea(compassRegion, chunk, i);
+        }
+    }
+
+    /**
+     * Notifies the compass service that a skystone block has either been placed or replaced at the give position.
+     */
+    public static void notifyBlockChange(ServerLevel level, BlockPos pos) {
+        ChunkAccess chunk = level.getChunk(pos);
+        var compassRegion = FMTTCompassRegion.get(level, chunk.getPos());
+        updateArea(compassRegion, chunk, level.getSectionIndex(pos.getY()));
+    }
+
+    private static void updateArea(FMTTCompassRegion compassRegion, ChunkAccess chunk, int sectionIndex) {
+        int cx = chunk.getPos().x;
+        int cz = chunk.getPos().z;
+
+        var section = chunk.getSections()[sectionIndex];
+        if (section.hasOnlyAir()) {
+            compassRegion.setHasFMTTBlock(cx, cz, sectionIndex, false);
+            return;
+        }
+
+        // Count how many skystone blocks there are
+        var desiredState = StarPieceBlock.STAR_PIECE_BLOCK.get().defaultBlockState();
+        var blockCount = new AtomicInteger(0);
+        section.getStates().count((state, count) -> {
+            if (state == desiredState) {
+                blockCount.getAndIncrement();
+            }
+        });
+        compassRegion.setHasFMTTBlock(cx, cz, sectionIndex, blockCount.get() > 0);
+    }
+
+    private static int dist(int ax, int az, int bx, int bz) {
+        final int up = (bz - az) * CHUNK_SIZE;
+        final int side = (bx - ax) * CHUNK_SIZE;
+
+        return up * up + side * side;
+    }
+
+    private static double rad(int ax, int az, int bx, int bz) {
+        final int up = bz - az;
+        final int side = bx - ax;
+
+        return Math.atan2(-up, side) - Math.PI / 2.0;
+    }
+
+}
