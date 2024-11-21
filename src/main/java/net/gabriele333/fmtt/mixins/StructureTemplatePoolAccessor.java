81bef8cd760c39cca1825f26882ce5d0c4f219ad
Gabriele-333 <gabriele3infinity@gmail.com>
2024-11-21 21:57:41 +0100

    Structures

---
+++ src/main/java/net/gabriele333/fmtt/mixins/StructureTemplatePoolAccessor.java
@@ -0,0 +1,44 @@
+package net.gabriele333.fmtt.mixins;/*
+ * This file is part of From Magic To Tech.
+ * Copyright (c) 2024, Gabriele_333, All rights reserved.
+ *
+ * From Magic To Tech is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * From Magic To Tech is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with From Magic To Tech.  If not, see <http://www.gnu.org/licenses/lgpl>.
+ */
+
+import com.mojang.datafixers.util.Pair;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.world.level.levelgen.structure.pools.StructurePoolElement;
+import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.List;
+
+@Mixin(StructureTemplatePool.class)
+public interface StructureTemplatePoolAccessor {
+    @Accessor
+    List<Pair<StructurePoolElement, Integer>> getRawTemplates();
+
+    @Accessor
+    @Mutable
+    void setRawTemplates(List<Pair<StructurePoolElement, Integer>> rawTemplates);
+
+    @Accessor
+    ObjectArrayList<StructurePoolElement> getTemplates();
+
+    @Accessor
+    @Mutable
+    void setTemplates(ObjectArrayList<StructurePoolElement> templates);
+}
