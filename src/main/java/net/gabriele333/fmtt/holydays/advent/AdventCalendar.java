bb5efb529ac379fba48b788dcfa3d9083d6d4c3b
Gabriele-333 <gabriele3infinity@gmail.com>
2024-12-01 22:30:43 +0100

    Advent?

---
+++ src/main/java/net/gabriele333/fmtt/holydays/advent/AdventCalendar.java
@@ -0,0 +1,51 @@
+package net.gabriele333.fmtt.holydays.advent;/*
+ * This file is part of From Magic To Tech.
+ * Copyright (c) 2024, Gabriele_333, All rights reserved.
+ *
+ * From Magic To Tech is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * From Magic To Tech is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with From Magic To Tech.  If not, see <http://www.gnu.org/licenses/lgpl>.
+ */
+
+
+
+
+import java.time.LocalDate;
+
+import net.gabriele333.fmtt.fmtt;
+import net.minecraft.server.level.ServerPlayer;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+
+@EventBusSubscriber(modid = fmtt.MOD_ID)
+public class AdventCalendar {
+
+    @SubscribeEvent
+    public static void onPlayerJoinServer(PlayerEvent.PlayerLoggedInEvent event) {
+        int day = getCurrentDayOfMonth();
+
+
+    }
+
+
+
+    public static int getCurrentDayOfMonth() {
+        LocalDate currentDate = LocalDate.now();
+        return currentDate.getDayOfMonth();
+    }
+
+    public static void triggerQuest(ServerPlayer player, long questID) {
+        //Shit to trigger the quest
+    }
+
+}
