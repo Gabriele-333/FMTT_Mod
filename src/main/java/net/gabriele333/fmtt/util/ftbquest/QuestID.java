1ee9d01f83742999e474ae6933921ebaeac09bb4
Gabriele-333 <gabriele3infinity@gmail.com>
2024-12-02 22:34:30 +0100

    IDs for quests

---
+++ src/main/java/net/gabriele333/fmtt/util/ftbquest/QuestID.java
@@ -0,0 +1,49 @@
+package net.gabriele333.fmtt.util.ftbquest;/*
+ * This file is part of From Magic To Tech.
+ * Copyright (c) 2024, Gabriele_333, All rights reserved.
+ *
+ * From Magic To Tech is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * From Magic To Tech is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with From Magic To Tech.  If not, see <http://www.gnu.org/licenses/lgpl>.
+ */
+
+public class QuestID {
+    public static String[] AdventIDS ={
+            "72DC75F83B251FBD",
+            "3AE7481C1AE57F0B",
+            "2741B53B8BEE6D09",
+            "58E6341BEBD0B698",
+            "5243EFD3F89C0BE8",
+            "580505154A798F62", //day 6
+            "7B934ADB139022F5",
+            "2724E713C63DA660",
+            "4F15ED4C44D5C4F2",
+            "313EE7CF9E4CDBDA",
+            "26B2E74143E6B870",
+            "53E5FC0F210DBA00", //day 12
+            "27A04BB6A936C458",
+            "77AC4243E0E9B0ED",
+            "7EB2039A0B1C625C",
+            "2A5FBAC2A75413DF",
+            "41324543EA881CD5",
+            "76F3030A565BCD7C", //day 18
+            "51C197FF2ECF5C84",
+            "028664C4EC4A5160",
+            "1F09178AA866B1B4",
+            "6B4AC584FD9E7327",
+            "517CE3894C09ABA3",
+            "3C63D48D02055B16", //day 24
+
+            "169893E1C16ACEFF"  //day 25
+
+    };
+}
