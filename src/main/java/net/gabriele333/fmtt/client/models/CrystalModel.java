107df7a30c8baa33ff2dde7e48c133cdab9aa4f4
Gabriele-333 <gabriele3infinity@gmail.com>
2025-03-28 22:49:53 +0100

    New model (not final)

--- src/main/java/net/gabriele333/fmtt/client/models/CrystalModel.java
+++ src/main/java/net/gabriele333/fmtt/client/models/CrystalModel.java
@@ -30,35 +30,43 @@ import net.minecraft.world.entity.Entity;
 
 public class CrystalModel<T extends Entity> extends EntityModel<T> {
     public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(ResourceLocation.fromNamespaceAndPath("fmtt", "crystal"), "main");
-    private final ModelPart maincube;
-    private final ModelPart frame1;
+    public final ModelPart maincube;
+    public final ModelPart frame1;
+    public final ModelPart frame2;
 
     public CrystalModel(ModelPart root) {
         this.maincube = root.getChild("maincube");
         this.frame1 = root.getChild("frame1");
+        this.frame2 = root.getChild("frame2");
     }
 
     public static LayerDefinition createBodyLayer() {
         MeshDefinition meshdefinition = new MeshDefinition();
         PartDefinition partdefinition = meshdefinition.getRoot();
 
-        PartDefinition maincube = partdefinition.addOrReplaceChild("maincube", CubeListBuilder.create(), PartPose.offset(0.0F, 24.0F, 0.0F));
+        PartDefinition maincube = partdefinition.addOrReplaceChild("maincube", CubeListBuilder.create(), PartPose.offset(0.0F, 8.0F, 0.0F));
 
-        PartDefinition cube_r1 = maincube.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(0, 0).addBox(-8.0F, -8.0F, -8.0F, 16.0F, 16.0F, 16.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -16.0F, 0.0F, -0.7854F, 0.0F, 0.7854F));
+        PartDefinition cube_r1 = maincube.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(0, 0).addBox(-8.0F, -8.0F, -8.0F, 16.0F, 16.0F, 16.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, -0.7854F, -0.7854F, 0.7854F));
 
-        PartDefinition frame1 = partdefinition.addOrReplaceChild("frame1", CubeListBuilder.create().texOffs(0, 32).addBox(-12.0F, -10.0F, 12.0F, 24.0F, 2.0F, 0.0F, new CubeDeformation(0.0F))
-                .texOffs(0, 38).addBox(-12.0F, -10.0F, -12.0F, 24.0F, 2.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+        PartDefinition frame1 = partdefinition.addOrReplaceChild("frame1", CubeListBuilder.create().texOffs(0, 42).addBox(-15.0F, -1.0F, 14.0F, 29.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 51).addBox(-14.0F, -1.0F, -14.0F, 29.0F, 2.0F, -1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 8.0F, 0.0F));
 
-        PartDefinition cube_r2 = frame1.addOrReplaceChild("cube_r2", CubeListBuilder.create().texOffs(0, 36).addBox(-16.0F, -10.0F, -1.0F, 24.0F, 2.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(13.0F, 0.0F, -4.0F, 0.0F, 1.5708F, 0.0F));
+        PartDefinition cube_r2 = frame1.addOrReplaceChild("cube_r2", CubeListBuilder.create().texOffs(0, 48).addBox(-21.0F, -10.0F, -1.0F, 29.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(15.0F, 9.0F, -6.0F, 0.0F, 1.5708F, 0.0F));
 
-        PartDefinition cube_r3 = frame1.addOrReplaceChild("cube_r3", CubeListBuilder.create().texOffs(0, 34).addBox(-16.0F, -10.0F, -1.0F, 24.0F, 2.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-13.0F, 0.0F, 4.0F, 0.0F, -1.5708F, 0.0F));
+        PartDefinition cube_r3 = frame1.addOrReplaceChild("cube_r3", CubeListBuilder.create().texOffs(0, 45).addBox(-19.0F, -10.0F, -1.0F, 29.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-15.0F, 9.0F, 4.0F, 0.0F, -1.5708F, 0.0F));
 
-        return LayerDefinition.create(meshdefinition, 64, 64);
+        PartDefinition frame2 = partdefinition.addOrReplaceChild("frame2", CubeListBuilder.create().texOffs(0, 32).addBox(-18.0F, -1.0F, 17.0F, 35.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 41).addBox(-17.0F, -1.0F, -17.0F, 35.0F, 2.0F, -1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 8.0F, 0.0F, 3.1416F, 0.0F, 0.0F));
+
+        PartDefinition cube_r4 = frame2.addOrReplaceChild("cube_r4", CubeListBuilder.create().texOffs(0, 38).addBox(-28.0F, -10.0F, -1.0F, 35.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(18.0F, 9.0F, -10.0F, 0.0F, 1.5708F, 0.0F));
+
+        PartDefinition cube_r5 = frame2.addOrReplaceChild("cube_r5", CubeListBuilder.create().texOffs(0, 35).addBox(-26.0F, -10.0F, -1.0F, 35.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-18.0F, 9.0F, 8.0F, 0.0F, -1.5708F, 0.0F));
+
+        return LayerDefinition.create(meshdefinition, 128, 128);
     }
 
     @Override
     public void setupAnim(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
-        // Animation logic here if needed
     }
 
     @Override
@@ -68,5 +76,6 @@ public class CrystalModel<T extends Entity> extends EntityModel<T> {
 
         maincube.render(poseStack, buffer, packedLight, packedOverlay);
         frame1.render(poseStack, buffer, packedLight, packedOverlay);
+        frame2.render(poseStack, buffer, packedLight, packedOverlay);
     }
 }
